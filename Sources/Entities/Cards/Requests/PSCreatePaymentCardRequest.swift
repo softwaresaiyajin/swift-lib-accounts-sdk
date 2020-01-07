8b60354afb36029cc6fb1904d9b7ed1eafea1c68
Pattio <Pattio@users.noreply.github.com>
2020-01-07 19:55:46 +0200

    Add visual type to PSCreatePaymentCardRequest (#27)

--- Sources/Entities/Cards/Requests/PSCreatePaymentCardRequest.swift
+++ Sources/Entities/Cards/Requests/PSCreatePaymentCardRequest.swift
@@ -8,18 +8,27 @@ public class PSCreatePaymentCardRequest: Mappable {
     public var accountNumber: String?
     public var accountOwnerId: Int?
     public var chargeInfo: PSChargeInfo?
+    public var visualType: String?
     
     required public init?(map: Map) {
     }
     
-    public init(cardOwnerId: Int, shippingAddress: PSPaymentCardShippingAddress, deliveryType: String, accountOwnerId:  Int? = nil, accountNumber: String? = nil, chargeInfo: PSChargeInfo?) {
-        
+    public init(
+        cardOwnerId: Int,
+        shippingAddress: PSPaymentCardShippingAddress,
+        deliveryType: String,
+        accountOwnerId: Int? = nil,
+        accountNumber: String? = nil,
+        chargeInfo: PSChargeInfo? = nil,
+        visualType: String? = nil
+    ) {
         self.cardOwnerId = cardOwnerId
         self.shippingAddress = shippingAddress
         self.accountOwnerId = accountOwnerId
         self.accountNumber = accountNumber
         self.chargeInfo = chargeInfo
         self.deliveryType = deliveryType
+        self.visualType = visualType
     }
     
     public func mapping(map: Map) {
@@ -29,5 +38,6 @@ public class PSCreatePaymentCardRequest: Mappable {
         chargeInfo      <- map["charge_info"]
         shippingAddress <- map["shipping_address"]
         deliveryType    <- map["delivery_type"]
+        visualType      <- map["visual_type"]
     }
 }
