59f4ae3509d337fe7036999bfa62a1c90232f642
Pattio <Pattio@users.noreply.github.com>
2020-02-25 12:48:26 +0200

    Update PSPaymentCardIssuePriceFilter (#32)

--- Sources/Entities/Cards/Requests/PSPaymentCardIssuePriceFilter.swift
+++ Sources/Entities/Cards/Requests/PSPaymentCardIssuePriceFilter.swift
@@ -1,13 +1,14 @@
-import Foundation
 import ObjectMapper
-import PayseraCommonSDK
 
-public class PSPaymentCardIssuePriceFilter: PSBaseFilter {
+public class PSPaymentCardIssuePriceFilter: Mappable {
     public var cardAccountOwnerId: String!
     public var cardOwnerId: String!
     
-    public override func mapping(map: Map) {
-        super.mapping(map: map)        
+    public init() {}
+    
+    required public init?(map: Map) { }
+    
+    public func mapping(map: Map) {
         cardAccountOwnerId      <- map["card_account_owner_id"]
         cardOwnerId             <- map["card_owner_id"]
     }
