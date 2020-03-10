78e176aa7fcd94e8f06d6ce3701067f0610d3976
Vytautas Gimbutas <v.gimbutas@paysera.com>
2020-03-10 21:14:20 +0200

    Alamofire 5 compatability

--- Sources/Services/AccountsApiClientFactory.swift
+++ Sources/Services/AccountsApiClientFactory.swift
@@ -8,11 +8,10 @@ public class AccountsApiClientFactory {
         tokenRefresher: PSTokenRefresherProtocol? = nil,
         logger: PSLoggerProtocol? = nil
     ) -> AccountsApiClient {
-        let sessionManager = SessionManager()
-        sessionManager.adapter = PSRequestAdapter(credentials: credentials)
+        let session = Session(interceptor: PSRequestAdapter(credentials: credentials))
     
         return AccountsApiClient(
-            sessionManager: sessionManager,
+            session: session,
             credentials: credentials,
             tokenRefresher: tokenRefresher,
             logger: logger
